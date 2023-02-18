086122766ddb0f34067c4bfed9ac5b26f980a5ac
Sean Lailvaux <142589+seanlail@users.noreply.github.com>
2023-02-18 18:31:12 +0000

    Create example

---
+++ src/lib/Button.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import type { HTMLButtonAttributes } from 'svelte/elements';
+
+	export let type: HTMLButtonAttributes['type'] = 'button';
+</script>
+
+<button {type}><slot /></button>
